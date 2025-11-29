8ffc0072f375561ae5663043cb83ce74f64884ba
chajmaa <chajmaa.bellakhal@aloysiusstichting.nl>
2025-11-29 08:56:01 +0100

    Added ClassicFilter-GlowUpStudio.png

--- src/data/filterData.js
+++ src/data/filterData.js
@@ -1,10 +1,38 @@
 const filterData = [
   {
-    id: 'retirement',
-    name: 'Happy Retirement',
-    description: 'Vier je pensioen met gouden vuurwerk en een persoonlijk bericht',
-    imageUrl: '/happy-retirement.png',
-    thumbnailUrl: '/happy-retirement.png'
+    id: 'classic',
+    name: 'Classic Filter',
+    description: 'Klassieke stijl voor je foto',
+    imageUrl: '/ClassicFilter-GlowUpStudio.png',
+    thumbnailUrl: '/ClassicFilter-GlowUpStudio.png'
+  },
+  {
+    id: 'deluxe',
+    name: 'Deluxe Filter',
+    description: 'Luxe uitstraling voor je speciale moment',
+    imageUrl: '/DeluxeFilter-GlowUpStudio.png',
+    thumbnailUrl: '/DeluxeFilter-GlowUpStudio.png'
+  },
+  {
+    id: 'examen',
+    name: 'Examen Feest',
+    description: 'Vier je geslaagd zijn met deze feestelijke overlay',
+    imageUrl: '/ExamenFeestFilter.png',
+    thumbnailUrl: '/ExamenFeestFilter.png'
+  },
+  {
+    id: 'glowup1',
+    name: 'GlowUp Style 1',
+    description: 'Moderne GlowUp effecten',
+    imageUrl: '/GlowUpStudioFilter1.png',
+    thumbnailUrl: '/GlowUpStudioFilter1.png'
+  },
+  {
+    id: 'glowup2',
+    name: 'GlowUp Style 2',
+    description: 'Trendy GlowUp vibes',
+    imageUrl: '/GlowUpstudioFilter2.png',
+    thumbnailUrl: '/GlowUpstudioFilter2.png'
   }
 ];
 
