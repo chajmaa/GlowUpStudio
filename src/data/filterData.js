aac327d4073aabeb7862e7e57f6bd9d5294d8bc2
chajmaa <chajmaa.bellakhal@aloysiusstichting.nl>
2025-11-29 08:41:05 +0100

    Updated filterData.js

--- src/data/filterData.js
+++ src/data/filterData.js
@@ -3,8 +3,8 @@ const filterData = [
     id: 'retirement',
     name: 'Happy Retirement',
     description: 'Vier je pensioen met gouden vuurwerk en een persoonlijk bericht',
-    imageUrl: '/retirement-filter.png',
-    thumbnailUrl: '/retirement-filter.png'
+    imageUrl: '/Pensioen-Bart.png',
+    thumbnailUrl: '/Pensioen-Bart.png'
   }
 ];
 
