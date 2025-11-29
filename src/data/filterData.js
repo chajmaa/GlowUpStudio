abf72c406df21785ed84af45571b792fc915b350
chajmaa <chajmaa.bellakhal@aloysiusstichting.nl>
2025-11-29 08:39:48 +0100

    Added retirement-filter.png

--- src/data/filterData.js
+++ src/data/filterData.js
@@ -1,31 +1,10 @@
 const filterData = [
   {
-    id: 'geslaagd',
-    name: 'Geslaagd 2025',
-    description: 'Vier je examen met deze feestelijke overlay',
-    imageUrl: 'https://i.ibb.co/pBr7dP5G/examen-1280-x-640-px-1280-x-1000-px-1280-x-1280-px-1.png',
-    thumbnailUrl: 'https://i.ibb.co/pBr7dP5G/examen-1280-x-640-px-1280-x-1000-px-1280-x-1280-px-1.png'
-  },
-  {
-    id: 'confetti',
-    name: 'Party Explosion',
-    description: 'Kleurrijke confetti-effecten',
-    imageUrl: 'https://i.ibb.co/cqpxtGW/Zonder-titel-1280-x-1280-px-7.png',
-    thumbnailUrl: 'https://i.ibb.co/cqpxtGW/Zonder-titel-1280-x-1280-px-7.png'
-  },
-  {
-    id: 'golden',
-    name: 'Golden Vibes',
-    description: 'Stralende gouden effecten voor je speciale moment',
-    imageUrl: 'https://i.ibb.co/1Yx1Z5BS/Examen-feest-1280-x-1280-px-kopie.png',
-    thumbnailUrl: 'https://i.ibb.co/1Yx1Z5BS/Examen-feest-1280-x-1280-px-kopie.png'
-  },
-  {
-    id: 'retro',
-    name: 'Retro Vibes',
-    description: 'Polaroid-stijl met vintage kleuren',
-    imageUrl: 'https://i.ibb.co/3qbgmxc/Examen-feest-1280-x-1280-px.png',
-    thumbnailUrl: 'https://i.ibb.co/3qbgmxc/Examen-feest-1280-x-1280-px.png'
+    id: 'retirement',
+    name: 'Happy Retirement',
+    description: 'Vier je pensioen met gouden vuurwerk en een persoonlijk bericht',
+    imageUrl: '/retirement-filter.png',
+    thumbnailUrl: '/retirement-filter.png'
   }
 ];
 
